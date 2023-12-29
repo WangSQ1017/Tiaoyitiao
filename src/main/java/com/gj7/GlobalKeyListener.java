9d10052f6776f5ef87e3ba1344c9c42cf64083e0
shuai_wang <shuai_wang@infinova.com.cn>
2023-12-29 17:36:28 +0800

    1. 优化，元旦快乐

--- src/main/java/com/gj7/GlobalKeyListener.java
+++ src/main/java/com/gj7/GlobalKeyListener.java
@@ -62,12 +62,19 @@ public class GlobalKeyListener implements NativeKeyListener {
 
     public static void main(String[] args) throws IOException {
         File file = new File("C:\\Users\\DELL\\Desktop\\新建文件夹\\1703822482279.png");
-        BufferedImage image=(BufferedImage)ImageIO.read(file);
+        BufferedImage image = (BufferedImage) ImageIO.read(file);
         analyzingImages(image);
     }
 
     public static void analyzingImages(BufferedImage bufferedImage) throws IOException {
-
+        int topX1 = 0;
+        int topY1 = 0;
+        int topX2 = 0;
+        int topY2 = 0;
+        int bottomX1 = 0;
+        int bottomY1 = 0;
+        int bottomX2 = 0;
+        int bottomY2 = 0;
         int width = bufferedImage.getWidth();
         int height = bufferedImage.getHeight();
         int minx = bufferedImage.getMinX();
@@ -75,8 +82,7 @@ public class GlobalKeyListener implements NativeKeyListener {
         System.out.println("width=" + width + ",height=" + height + ".");
         System.out.println("minx=" + minx + ",miniy=" + miny + ".");
         int[] rgb = new int[3];
-        int[] tempXY = new int[4];
-        int[] realXY = new int[4];
+
         int flag = 0;
         boolean lastLine = false;
         for (int i = minx; i < height; i++) {
@@ -87,18 +93,68 @@ public class GlobalKeyListener implements NativeKeyListener {
                 rgb[1] = (pixel & 0xff00) >> 8;
                 rgb[2] = (pixel & 0xff);
                 String s = rgb[0] + "" + rgb[1] + "" + rgb[2];
-                if (colorList.contains(Integer.parseInt(s))){
+                if (colorList.contains(Integer.parseInt(s))) {
+                    if (flag == 0) {
+                        if (count == 0) {
+                            topX1 = j;
+                            topY1 = i;
+                        }
+                        count++;
+                    }
+                    if (flag == 2) {
+                        if (count == 0) {
+                            bottomX1 = j;
+                            bottomY1 = i;
+                        }
+                        count++;
+                    }
 
                 } else {
+                    if (flag == 0) {
+                        if (count >= 3) {
+                            topX2 = j - 1;
+                            topY2 = i;
+                            flag = 2;
+                            i += 30;
+                            break;
+                        } else {
+                            count = 0;
+                            topX1 = 0;
+                            topY1 = 0;
+                        }
+                    }
 
+                    if (flag == 2) {
+                        if (j == width - 1) {
+                            if (count == 0 && lastLine) {
+                                i += 100000;
+                            }
+                        }
 
+                        if (count >= 3) {
+                            bottomX2 = j - 1;
+                            bottomY2 = i;
+                            lastLine = true;
+                            break;
+                        } else {
+                            count = 0;
+                            bottomX1 = 0;
+                            bottomY1 = 0;
+                        }
+
+                    }
                 }
 
             }
         }
-        bufferedImage.setRGB((tempXY[0] + tempXY[2]) / 2, (tempXY[1] + tempXY[3]) / 2, new Color(255,0,0).getRGB());
-        bufferedImage.setRGB(tempXY[0], tempXY[1], new Color(255,0,0).getRGB());
-        bufferedImage.setRGB(tempXY[2], tempXY[3], new Color(255,0,0).getRGB());
+        System.out.println(topX1 + "," + topY1);
+        System.out.println(topX2 + "," + topY2);
+        System.out.println(bottomX1 + "," + bottomY1);
+        System.out.println(bottomX2 + "," + bottomY2);
+        bufferedImage.setRGB(topX1, topY1, new Color(255, 0, 0).getRGB());
+        bufferedImage.setRGB(topX2, topY2, new Color(255, 0, 0).getRGB());
+        bufferedImage.setRGB(bottomX1, bottomY1, new Color(255, 0, 0).getRGB());
+        bufferedImage.setRGB(bottomX2, bottomY2, new Color(255, 0, 0).getRGB());
         File file = new File("C:\\Users\\DELL\\Desktop\\新建文件夹\\" + System.currentTimeMillis() + ".png");
         ImageIO.write(bufferedImage, "png", file);
     }
